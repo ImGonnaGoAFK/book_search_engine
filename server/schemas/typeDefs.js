f9214119d4926f624f72dd6910054fb43717963b
Kevin <kevhoang97@gmail.com>
2024-08-17 15:45:02 -0400

    worked with server folder

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,22 +3,26 @@ type User {
     _id: ID
     username: String
     email: String
-    savedBooks: [Book]!
     bookCount: Int
+    savedBooks: [Book]!
 }
 
 type Book {
-    _id: ID
+    bookId: String
     authors: [String]
     description: String
+    title: String
+    image: String
+    link: String
+}
+
+input BookInput {
+    authors: [String]
+    description: String
+    title: String
     bookId: String
     image: String
     link: String
-    title: String
-}
-
-type Author {
-    name: String
 }
 
 type Auth {
@@ -34,4 +38,12 @@ type Query {
     me: User
 }
 
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput!): User
+    removeBook(bookId: String!): User
+}
 `;
+
+module.exports = typeDefs;
